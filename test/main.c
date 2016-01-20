7398d9c2af90ac931d6a03366abe709598c7e7db
Venkatesh <venkateshmadhava@venkateshs-macbook-pro.local>
2016-01-20 14:47:23 +0530

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Venkatesh Madhava on 20/01/16.
+//  Copyright © 2016 Venkatesh Madhava. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
