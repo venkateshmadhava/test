5aaef3144493d5539791a1051278ad88ca96f44a
venkateshmadhava <venkateshmadhava@Venkateshs-MacBook-Pro-3.local>
2016-12-28 21:01:30 +0530

    hi

--- test/main.c
+++ test/main.c
@@ -7,9 +7,25 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    int i;
+    int j;
+    int a;
+    i=11;
+    j=10;
+    printf("%d",i);
+    
+    for (a=0; a<11; a++)
+    {
+        printf("%d",a);
+        
+    }
+    
+
+
     return 0;
 }
